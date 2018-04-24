e1d5c43767d3e280ac2f407f181582896d0c6e1f
Vicente Ternicien <38274115+vicho2t@users.noreply.github.com>
2018-04-24 19:20:06 -0300

    update

--- Clase de Java/src/Projecto/TaskList.java
+++ Clase de Java/src/Projecto/TaskList.java
@@ -3,11 +3,28 @@ package Projecto;
 public class TaskList {
 
 	public Task[] List = new Task[100]; //Define la lista de tareas
+	//String[] Task = new String[100];
 		
-	public static TaskList AddTask(TaskList lista, String s) {
+	public TaskList AddTask(TaskList lista, String s) {
 		
-		Task t1  = new Task();
-		t1.Message = s;
+		//Task t1  = new Task();
+		//t1.Message = s;
+		
+		if (lista.List[0].Message == null) {
+			
+			lista.List[0].Message = s;
+			
+			return lista;
+		}
+		
+		
+		for (int i = 0; lista.List[i].Message != null; i++) {
+			
+			if (lista.List[i].Message == null)
+			
+			
+			if(i == 99) break;
+		}
 		
 		return lista;
 	}
